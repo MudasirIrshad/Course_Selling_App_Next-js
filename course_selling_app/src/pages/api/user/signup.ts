8e455ab35200895273c64fcb91574a3272e27ec2
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-12-24 12:00:00 +0500

    Your commit message

---
+++ course_selling_app/src/pages/api/user/signup.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "../database/db";
+import { corsMiddleware } from "@/lib/cors";
+import { UserZodSchema, SECRET_KEY } from "@/lib/util";
+import jwt from "jsonwebtoken";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method == "POST") {
+    corsMiddleware(req, res);
+
+    const inputs = UserZodSchema.safeParse(req.body);
+    if (inputs.error) res.send({ message: inputs.error });
+
+    if (!inputs.success) res.send("issue in input data");
+    let username = inputs.data?.username;
+    let gmail = inputs.data?.gmail;
+    let password = inputs.data?.password;
+    let findAdmin = await User.findOne({ gmail });
+    if (findAdmin) res.send({ message: "User already exits" });
+    else {
+      const newUser = new User({
+        username,
+        gmail,
+        password,
+      });
+      await newUser.save();
+      let token = jwt.sign({ username, gmail }, SECRET_KEY);
+      res.send({ message: "User Signup Token", token });
+    }
+  }
+}
